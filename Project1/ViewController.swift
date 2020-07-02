17d66a17d2aa1d7584e7c37245be9e00075a580f
Chawan Saeed <chawansaeed@Chawans-MacBook-Air.local>
2020-07-02 21:45:40 +0300

    Initial commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Chawan Saeed on 7/2/20.
+//  Copyright © 2020 Chawan Saeed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    @IBOutlet weak var counterLabel: UILabel!
+    var counter = 0
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func minusCounter(sender: AnyObject) {
+     counterLabel.text = "\(counter - 1)"
+        counter--
+    }
+    
+    
+    @IBAction func counter(sender: AnyObject) {
+        counterLabel.text = "\(counter + 1)"
+        counter++
+    }
+
+
+    
+}
+
